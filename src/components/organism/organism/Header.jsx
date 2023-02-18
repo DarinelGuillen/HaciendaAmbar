8ae4f0eed65c65e84cce208511ac21f0556dda04
Darioantonio20 <antoniodario10@hotmail.com>
2023-02-17 21:28:39 -0600

    [FIX] Just better the css in header

--- src/components/organism/organism/Header.jsx
+++ src/components/organism/organism/Header.jsx
@@ -1,26 +1,29 @@
 import { Link } from "react-router-dom";
 import Label from "../atoms/Label";
 import Icon from "../atoms/Icon"
-import logoColor from "../../../assets/img/logoColor.png"
+import "../../../assets/img/logoColor.png"
 import "../../../assets/style/Header.css"
 function Header() {  
     return (
       <>
-       <div className="iconoAmbar">
-          <Icon icon={logoColor}></Icon>
-       </div>
-       <div className="NavBarInicioSesion">
-          <Label label={"Iniciar sesión"}></Label>
-       </div>
-       <div className="NavBarTipoReserva">
-          <Label label={"Tipos de reserva"}></Label>
-       </div>
-       <div className="NavBarCorrexto">
-          <Label label={"Contacto"}></Label>
-       </div>
-       <div className="NavBarAcercaNosotros">
-          <Label label={"Acerca de nosotros"}></Label>
-       </div>
+      <div className="navBar">
+         <div className="iconoAmbar">
+            <img src="src/assets/img/LogoColor.png"></img>
+         </div>
+         <div className="pruebita">
+
+            <Label label={"Iniciar sesión"}></Label>
+            <Label label={"Tipos de reserva"}></Label>
+            <Label label={"Contacto"}></Label>
+            <Label label={"Acerca de nosotros"}></Label>
+       
+         </div>
+ 
+      </div>
+
+
+
+
 
       <div className="links">
         <Link to="/Login">
@@ -35,4 +38,4 @@ function Header() {
     );
   }
   export default Header;
-  
+  //<Icon icon={logoColor} ></Icon>
