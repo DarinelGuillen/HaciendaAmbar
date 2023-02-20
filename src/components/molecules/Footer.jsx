fdede5daae4b43f2f3f39ddba1c9e52d1654e888
Darioantonio20 <antoniodario10@hotmail.com>
2023-02-20 13:44:39 -0600

    [FIX] The footer structure and modify responsive status

--- src/components/molecules/Footer.jsx
+++ src/components/molecules/Footer.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
-import iconoFooter from "../../assets/img/iconoBlancoNegro.png"
-import iconoRedes from "../../assets/img/IconoRedes.png"
+import iconoFooter from "../../assets/img/logoBlancoNegro.svg"
+import iconoRedes from "../../assets/img/IconoRedes.svg"
 import Icon from "../atoms/Icon"
 import Label from "../atoms/Label"
 import "../../assets/style/Footer.css"
@@ -8,32 +8,26 @@ import "../../assets/style/Footer.css"
 function Footer() {
   return (
     <>
-      <div className="formaFooter">
-        <div className="iconFooter">
-          <Icon icon={iconoFooter}/>
-            <div className="labelFooterSlogan">
-              <Label label={"Recuerdos para siempre"}/>
-            </div>
-        </div>
-        <div className="iconRedes">
-          <Icon icon={iconoRedes}/>
-            <div className="labelFooter3">
-              <Label label={"© 2022 Hacienda Ambar"}/>
-            </div>
-            <div className="labelFooterConjunto1">
-              <Label label={"Empresa"}/>
-              <Label label={"Acerca de"}/>
-              <Label label={"Empleo"}/>
-            </div>
-            <div className="labelFooterConjunto2">
-              <Label label={"Enlaces útiles "}/>
-              <Label label={"Ayuda"}/>
-              <Label label={"Redes sociales"}/>
-            </div>
-        </div>
+    <div className="Footer">
+      <div className="iconoFooter">
+          <Icon icon={iconoFooter}/>  
+          <Label label={"Recuerdos para siempre"}/>
       </div>
-      
-     
+      <div className="informacion">
+        <Label label={"Empresa"}/>
+        <Label label={"Acerca de"}/>
+        <Label label={"Empleo"}/>
+      </div>
+      <div className="enlaces">
+        <Label label={"Enlaces útiles "}/>
+        <Label label={"Ayuda"}/>
+        <Label label={"Redes sociales"}/>
+      </div>
+      <div className="redesFooter">
+          <Icon icon={iconoRedes}/>
+          <Label label={"© 2022 Hacienda Ambar"}/>
+      </div>
+    </div>
     </>
   );
 }
--- src/components/molecules/RecuerdosParaSiempre.jsx
+++ src/components/molecules/RecuerdosParaSiempre.jsx
@@ -4,18 +4,22 @@ import Label from "../atoms/Label";
 function RecuerdosParaSiempre() {
     return ( 
         <>
-            <div className="cardFondo">
+           <main>
                 <div className="tituloRecuerdosParaSiempre">
+                <div className="titulio">
+                    
                     <Label label={"Los 5 mandamientos del invitado"}></Label>
                 </div>
-                <ul className="tablaNoOrdenada">
+                <ul>
                     <li type="disc">Abrirás la invitación cuando te la entreguen.</li><br></br>
                     <li type="disc">Serás puntual y llegarás a la ceremonia.</li><br></br>
                     <li type="disc">No pedirás un +1.</li><br></br>
                     <li type="disc">No te molestarás si tus niños no estás invitados (es por tu bien).</li><br></br>
                     <li type="disc">Confirmarás asistencia lo antes posible y no cancelarás de último momento.</li>
                 </ul>
-            </div>
+                </div>
+           </main>
+            
         </>
      );
 }
