a695dbd8eac9459e7dddc5418040226e88841b9d
DarinelGuillen <darinel.escobar@outlook.com>
2023-02-23 13:59:31 -0600

    [ADD] Header if loged=true or false

--- src/components/molecules/Header.jsx
+++ src/components/molecules/Header.jsx
@@ -1,8 +1,16 @@
 import { Link } from "react-router-dom";
+import {  useContext } from "react";
+
 import Label from "../atoms/Label";
+import UserContext from "../../contexts/UserContext";
+
 import "../../assets/img/LogoColor.png";
 import "../../assets/style/Header.css";
+
+
 function Header() {
+  const { isLoged, setIsLoged } = useContext(UserContext);
+
   return (
     <>
       <div className="navBar">
@@ -10,8 +18,14 @@ function Header() {
           <img src="src/assets/img/LogoColor.png"></img>
         </div>
         <div className="pruebita">
-          <Link to="/Login">Iniciar sesión</Link>
-          <Link to="/SingIn">Registrate</Link>
+          {!isLoged ? (
+            <>
+            <Link to="/Login">Iniciar sesión</Link>
+            <Link to="/SingIn">Registrate</Link>
+            </>
+          ) : (
+            <Link to="/">EJEMPLO 'log out' or IMG</Link>
+          )}
           <Label label={"Tipos de reserva"}></Label>
           <Label label={"Contacto"}></Label>
           <Label label={"Acerca de nosotros"}></Label>
@@ -23,5 +37,30 @@ function Header() {
     </>
   );
 }
+
 export default Header;
-//<Icon icon={logoColor} ></Icon>
+
+// function Header() {
+
+//   return (
+//     <>
+//       <div className="navBar">
+//         <div className="iconoAmbar">
+//           <img src="src/assets/img/LogoColor.png"></img>
+//         </div>
+//         <div className="pruebita">
+//           <Link to="/Login" >Iniciar sesión</Link>
+//           <Link to="/SingIn">Registrate</Link>
+//           <Label label={"Tipos de reserva"}></Label>
+//           <Label label={"Contacto"}></Label>
+//           <Label label={"Acerca de nosotros"}></Label>
+//         </div>
+//       </div>
+
+//       <br />
+//       <Link to="/General">General</Link>
+//     </>
+//   );
+// }
+// export default Header;
+// //<Icon icon={logoColor} ></Icon>
