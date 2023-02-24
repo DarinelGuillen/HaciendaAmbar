5f2224688208b780b786eef5de32a190551b8e58
Darioantonio20 <antoniodario10@hotmail.com>
2023-02-24 13:13:48 -0600

    [ADD] table to Admin with a database conection

--- src/components/molecules/TestAdminView.jsx
+++ src/components/molecules/TestAdminView.jsx
@@ -7,23 +7,38 @@ function TestAdminView() {
         <table class="default">
             <tr>
                 <th>Usuario</th>
-                <th>Mañana</th>
-                <th>Domingo</th>
+                <th>Nombre de paquete</th>
+                <th>Inicio de renta</th>
+                <th>Fin de renta</th>
+                <th>Costo</th>
+                
             </tr>
             <tr>
-                <td>{}</td>
-                <td>...</td>
-                <td>...</td>
+                <td>Dario Antonio</td>
+                <td>Albercazo Ámbar</td>
+                <td>24/02/2023</td>
+                <td>24/02/2023</td>
+                <td>$2,300</td>
+                <td><button>Aceptar Renta</button></td>
+                <td><button>Denegar Renta</button></td>
             </tr>
             <tr>
-                <td>......</td>
-                <td>......</td>
-                <td>......</td>
+                <td>José Merliny Linguini</td>
+                <td>Paquete Gold 200</td>
+                <td>26/02/2023</td>
+                <td>27/02/2023</td>
+                <td>$4,300</td>
+                <td><button>Aceptar Renta</button></td>
+                <td><button>Denegar Renta</button></td>
             </tr>
             <tr>
-                <td>.........</td>
-                <td>.........</td>
-                <td>.........</td>
+                <td>Darinel de Jesús Ochoa</td>
+                <td>Paquete Platinum 200</td>
+                <td>28/02/2023</td>
+                <td>29/02/2023</td>
+                <td>$10,200</td>
+                <td><button>Aceptar Renta</button></td>
+                <td><button>Denegar Renta</button></td>
             </tr>
         </table>
         </>
