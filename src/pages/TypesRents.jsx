2e760529ae1c2a4a5146e75a102542cca7b3e3af
Darioantonio20 <antoniodario10@hotmail.com>
2023-03-18 17:57:24 -0600

    [ADD] the footer to typesRent

--- src/pages/TypesRents.jsx
+++ src/pages/TypesRents.jsx
@@ -1,11 +1,13 @@
 import Header from "../components/molecules/Header";
 import TipodeReservaView from "../components/organism/TipodeReservaView";
+import Footer from "../components/molecules/Footer";
 
 function TypesRents() {
     return ( 
         <>
         <Header/>
         <TipodeReservaView/>
+        <Footer></Footer>
         
         </>
      );
