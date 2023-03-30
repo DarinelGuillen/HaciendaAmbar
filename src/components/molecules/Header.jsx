fbe1506ad41a0d7abed2deac8a2b52a96c0a3499
Darioantonio20 <antoniodario10@hotmail.com>
2023-03-29 21:03:54 -0600

    [FIX] the navBar with de new bootstrap with CDN in the proyect

--- src/components/molecules/Header.jsx
+++ src/components/molecules/Header.jsx
@@ -17,16 +17,15 @@ function Header() {
   }
   return (
     <>
-
-      <nav className="navbar navbar-expand-lg">
-        <div className="container-fluid">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
           <Link to="/">
-            <a className="navbar-brand">
+            <a className="navbar-brand ">
               <img loading="lazy" className="logoNavbar" src="src/assets/img/LogoColor.png"></img>
             </a>
           </Link>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
+          <button className="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon "></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav mx-auto">
